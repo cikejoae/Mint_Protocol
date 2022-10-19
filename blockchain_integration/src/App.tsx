e7fcf08fb256288142ac8474cf396ba3c68b62f5
A Mon <a.mont.24.777@gmail.com>
2022-10-19 15:19:21 -0500

    Agregando estilos y efectos CSS

--- blockchain_integration/src/App.tsx
+++ blockchain_integration/src/App.tsx
@@ -1,8 +1,7 @@
 import { useApi, useBalanceSubscription, useLoggedInAccount } from '@gear-js/react-hooks';
 import { Routing } from 'pages';
-import { Header, Footer, ApiLoader } from 'components';
+import { Header, ApiLoader } from 'components';
 import { withProviders } from 'hocs';
-import 'App.scss';
 
 function Component() {
   const { isApiReady } = useApi();
@@ -14,7 +13,6 @@ function Component() {
     <>
       <Header isAccountVisible={isLoginReady} />
       <main>{isApiReady && isLoginReady ? <Routing /> : <ApiLoader />}</main>
-      <Footer />
     </>
   );
 }
--- blockchain_integration/src/index.tsx
+++ blockchain_integration/src/index.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import { App } from './App';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container as HTMLElement);
 
 root.render(
   <StrictMode>
+    <ChakraProvider>
     <App />
+    </ChakraProvider>
   </StrictMode>,
 );
