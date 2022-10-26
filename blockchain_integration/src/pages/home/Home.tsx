db4383e11ae5a4bed9368457fc53a67900f3d0c9
A Mon <a.mont.24.777@gmail.com>
2022-10-26 14:46:50 -0500

    Agregando button  minteo en el main

--- blockchain_integration/src/pages/home/Home.tsx
+++ blockchain_integration/src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import React, {MouseEventHandler, MouseEvent , useState,} from 'react';
 import { Button as But,Flex, Heading, Box, HStack, CircularProgress, CircularProgressLabel,Tabs, TabList, TabPanels, Tab, TabPanel, VStack, Stack, Spacer,ButtonGroup,Center, ButtonProps, useColorModeValue, Tooltip } from '@chakra-ui/react';
+import { Portal, Popover, PopoverTrigger, PopoverContent, PopoverHeader, PopoverBody, PopoverFooter, PopoverArrow, PopoverCloseButton, PopoverAnchor,} from '@chakra-ui/react';
 import { FaRedhat } from "react-icons/fa";
 import { useEffect } from 'react';
 import { truncate } from 'fs';
@@ -74,6 +75,7 @@ function Home() {
 
 
  //////STATES PANEL
+
  const [beard, setBeard]= useState(false);
     
  const beardbutton = () => {
@@ -85,7 +87,7 @@ function Home() {
 
   const beardbut = beard ? 0 : 1
 
-  function beardPanel () {if (beardbut == 0){return(<img src={"./icons/BEARD.png"}  ></img>)};}
+  function beardPanel () {if (beardbut == 0 ){return(<img src={"./icons/BEARD.png"}  ></img>)};}
 
 
 
@@ -100,7 +102,7 @@ function Home() {
 
   const hatbut = hat ? 0 : 1
 
-  function hatPanel () {if (hatbut == 0){return(<img src={"./icons/HAT.png"}  ></img>)};}
+  function hatPanel () {if (hatbut == 0 ){return(<img src={"./icons/HAT.png"}  ></img>)};}
 
 
 
@@ -118,7 +120,7 @@ function Home() {
 
   const focusbut = focus ? 0 : 1
 
-  function focusPanel () {if (focusbut == 0){return(<img src={"./icons/FOCUS.png"}  ></img>)};}
+  function focusPanel () {if (focusbut == 0 ){return(<img src={"./icons/FOCUS.png"}  ></img>)};}
 
 
 
@@ -134,7 +136,7 @@ function Home() {
 
   const glassesbut = glasses ? 0 : 1
 
-  function glassesPanel () {if (glassesbut == 0){return(<img src={"./icons/GLASSES.png"}  ></img>)};}
+  function glassesPanel () {if (glassesbut == 0 ){return(<img src={"./icons/GLASSES.png"}  ></img>)};}
 
 
 
@@ -150,7 +152,7 @@ function Home() {
 
   const mustachebut = mustache ? 0 : 1
 
-  function mustachePanel () {if (mustachebut == 0){return(<img src={"./icons/MUSTACHE.png"}  ></img>)};}
+  function mustachePanel () {if (mustachebut == 0  ){return(<img src={"./icons/MUSTACHE.png"}  ></img>)};}
 
 
 
@@ -168,7 +170,7 @@ function Home() {
 
   const polkadotbut = polkadot ? 0 : 1
 
-  function polkadotPanel () {if (polkadotbut == 0){return(<img src={"./icons/POLKADOT.png"}  ></img>)};}
+  function polkadotPanel () {if (polkadotbut == 0 ){return(<img src={"./icons/POLKADOT.png"}  ></img>)};}
 
 
 
@@ -184,7 +186,7 @@ function Home() {
 
   const polkabut = polka ? 0 : 1
 
-  function polkaPanel () {if (polkabut == 0){return(<img src={"./icons/POLKA.png"}  ></img>)};}
+  function polkaPanel () {if (polkabut == 0  ){return(<img src={"./icons/POLKA.png"}  ></img>)};}
 
 
  const [gavin, setGavin]= useState(false);
@@ -198,7 +200,7 @@ function Home() {
 
   const gavinbut = gavin ? 0 : 1
 
-  function gavinPanel () {if (gavinbut == 0){return(<img src={"./icons/GAVIN_NEW.png"}  ></img>)};}
+  function gavinPanel () {if (gavinbut == 0 ){return(<img src={"./icons/GAVIN_NEW.png"}  ></img>)};}
 
    
 
@@ -215,7 +217,7 @@ function Home() {
    
      const pigbut = pig ? 0 : 1
 
-     function pigPanel () {if (pigbut == 0){return(<img src={"./icons/COCHINO.png"}  ></img>)};}
+     function pigPanel () {if (pigbut == 0 ){return(<img src={"./icons/COCHINO.png"}  ></img>)};}
   
   const [clown, setClown]= useState(false);
     
@@ -229,6 +231,9 @@ function Home() {
       const clownbut = clown ? 0 : 1
  
       function clownPanel () {if (clownbut == 0){return(<img src={"./icons/CLOWN.png"}  ></img>)};}
+
+    
+      
 
    
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -509,17 +514,17 @@ return (
       <Heading size="md">Filters</Heading>
         <HStack>
         <Flex gap="2">
-        <Box  border='2px' borderColor="#e80b9d" borderRadius='md' bg="white" w="100px" h="100px">
-         {pigPanel () }{clownPanel ()}{gavinPanel ()}{polkaPanel ()}{polkadotPanel()}{mustachePanel()}{glassesPanel()}{focusPanel()}{hatPanel()}{beardPanel()}
+        <Box  border='2px' borderColor="white" borderRadius='md' bg="white" w="80px" h="80px">
+         <HStack>
+          {pigPanel () } {clownPanel ()} {gavinPanel ()} {polkaPanel ()} {polkadotPanel()} {mustachePanel()}{glassesPanel()}{focusPanel()} {hatPanel()}{beardPanel()}
+ 
+         </HStack>
         </Box>
-        <Box border='2px' borderColor="#e80b9d" borderRadius='md' bg="white" w="100px" h="100px">
-        {pigPanel () }   
-        </Box>
-        <Box border='2px' borderColor="#e80b9d" borderRadius='md' bg="white" w="100px" h="100px">    
-        </Box>
+        <Box w="300px" h="40px"></Box>
+        
         </Flex>
         </HStack>
-            
+        <Box bg="white" w="300px" h="20px"></Box>   
         <Heading size="md">Styles</Heading>
         <HStack>
         <Flex gap="2">
@@ -541,8 +546,28 @@ return (
          <img  width="100%" height="100%" src={"./photo.jpg"}  ></img>
 
       <Box  w="100%" h="50px">
-      <But  colorScheme= "pink" w="100%" h="50px" bg= "#e80b9d"  type="submit"> Mint </But>      
+      <Popover>
+            <PopoverTrigger>
+            <But  colorScheme= "pink" w="100%" h="50px" bg= "#e80b9d"  type="submit"> Mint </But>     
+            </PopoverTrigger>
+            <Portal>
+            <PopoverContent>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverBody>
+            <HStack>
+              <Flex gap="6">
+             <But w="120px" h="50px" colorScheme='red'>NFT </But>
+             <But w="120px" h="50px" colorScheme='red'>RMRK</But>
+             </Flex>
+             </HStack>
+              </PopoverBody>
+             </PopoverContent>
+              </Portal>
+              </Popover>
+       
       </Box>
+       <But  colorScheme= "pink" w="100%" h="50px" bg= "#e80b9d"  type="submit"> Add features </But>
       </Box>
       <Spacer />
       </Flex>
@@ -567,70 +592,70 @@ return (
 
           <Box className="aumento"  w="100px" h="100px">
             <Tooltip label="Nose pig" aria-label='A tooltip' placement='top-start'>
-              <But bg ="white" w="100px" h="100px"  onClick = {(e)=> { pigbutton(); Multistate2(1) ;Multistate(1); activeInactive_button_nose_pork;}}  type="submit" > 
+              <But bg ="white" w="100px" h="100px"  onClick = {(e)=> { pigbutton(); activeInactive_button_nose_pork;}}  type="submit" > 
               <img src={"./icons/COCHINO.png"}  ></img>
               </But>
             </Tooltip>
           </Box>
           <Box  className="aumento" w="150px" h="150px">
             <Tooltip label="Nose clown" aria-label='A tooltip' placement='top-start'>
-              <But bg ="white" w="100px" h="100px" onClick = {(e)=> {clownbutton();Multistate2(2); Multistate(2); activeInactive_button_nose_clown}} type="submit"> 
+              <But bg ="white" w="100px" h="100px" onClick = {(e)=> {clownbutton(); activeInactive_button_nose_clown}} type="submit"> 
                <img src={"./icons/CLOWN.png"}  ></img>
               </But>
             </Tooltip>
           </Box>
           <Box  className="aumento" w="100px" h="100px">
             <Tooltip label="Mask Gavin" aria-label='A tooltip' placement='top-start'>
-              <But bg ="white" w="100px" h="100px" onClick = {(e)=> {gavinbutton (); Multistate2(3);Multistate(3); activeInactive_button_gavin}}  type="submit">
+              <But bg ="white" w="100px" h="100px" onClick = {(e)=> {gavinbutton (); activeInactive_button_gavin}}  type="submit">
               <img src={"./icons/GAVIN_NEW.png"}  ></img>
               </But>
             </Tooltip>
           </Box> 
           <Box className="aumento"  w="100px" h="100px">
             <Tooltip label="Polkadot" aria-label='A tooltip' placement='top-start'>
-              <But bg ="white" w="100px" h="100px" onClick = {(e)=> {polkabutton() ;Multistate2(4);Multistate(4);activeInactive_button_dot} } type="submit"> 
+              <But bg ="white" w="100px" h="100px" onClick = {(e)=> {polkabutton() ;activeInactive_button_dot} } type="submit"> 
               <img src={"./icons/POLKA.png"}  ></img> 
               </But>
             </Tooltip>
           </Box>
           <Box className="aumento" w="100px" h="100px">
             <Tooltip label="Polkadot" aria-label='A tooltip' placement='top-start'>
-              <But bg ="white" w="100px" h="100px" onClick = {(e)=> {polkadotbutton() ;Multistate2(5);Multistate(5); activeInactive_button_dot_2}}  type="submit"> 
+              <But bg ="white" w="100px" h="100px" onClick = {(e)=> {polkadotbutton() ; activeInactive_button_dot_2}}  type="submit"> 
               <img src={"./icons/POLKADOT.png"}  ></img> 
               </But>
             </Tooltip>
           </Box>
           <Box className="aumento" w="100px" h="100px">
             <Tooltip label="Mustache" aria-label='A tooltip' placement='top-start'>
-              <But bg ="white" w="100px" h="100px" onClick ={(e)=> {mustachebutton() ;Multistate2(6);Multistate(6); activeInactive_button_mustache}}  type="submit"> 
+              <But bg ="white" w="100px" h="100px" onClick ={(e)=> {mustachebutton(); activeInactive_button_mustache}}  type="submit"> 
               <img src={"./icons/MUSTACHE.png"}  ></img> 
               </But>
             </Tooltip>
           </Box>
           <Box className="aumento" w="100px" h="100px">
             <Tooltip label="Glasses" aria-label='A tooltip' placement='top-start'>
-              <But bg ="white" w="100px" h="100px" onClick = {(e)=> {glassesbutton(); Multistate2(7); Multistate(7); activeInactive_button_glasses}}  type="submit"> 
+              <But bg ="white" w="100px" h="100px" onClick = {(e)=> {glassesbutton();  activeInactive_button_glasses}}  type="submit"> 
               <img src={"./icons/GLASSES.png"}  ></img> 
               </But>
               </Tooltip>
           </Box>
           <Box  className="aumento" w="100px" h="100px">
             <Tooltip label="Focus" aria-label='A tooltip' placement='top-start'>
-              <But bg ="white" w="100px" h="100px" onClick =  {(e)=>  {focusbutton(); Multistate2(8); Multistate(8); activeInactive_button_focus}}  type="submit"> 
+              <But bg ="white" w="100px" h="100px" onClick =  {(e)=>  {focusbutton(); activeInactive_button_focus}}  type="submit"> 
               <img src={"./icons/FOCUS.png"}  ></img> 
               </But>
             </Tooltip>
           </Box>
           <Box className="aumento" w="100px" h="100px">
             <Tooltip label="Hat" aria-label='A tooltip' placement='top-start'>
-              <But bg ="white" w="100px" h="100px" onClick =  {(e)=>  {hatbutton(); Multistate2(9);Multistate(9); activeInactive_button_hat}}  type="submit"> 
+              <But bg ="white" w="100px" h="100px" onClick =  {(e)=>  {hatbutton(); activeInactive_button_hat}}  type="submit"> 
               <img src={"./icons/HAT.png"}  ></img> 
               </But>
             </Tooltip>
           </Box>
           <Box  className="aumento" w="100px" h="100px">
             <Tooltip label="Beard" aria-label='A tooltip' placement='top-start'>
-               <But bg ="white" w="100px" h="100px" onClick = {(e)=> {beardbutton(); Multistate2(10);Multistate(10); activeInactive_button_beard}}  type="submit"> 
+               <But bg ="white" w="100px" h="100px" onClick = {(e)=> {beardbutton();  activeInactive_button_beard}}  type="submit"> 
                <img src={"./icons/BEARD.png"}  ></img> 
               </But>
             </Tooltip>
