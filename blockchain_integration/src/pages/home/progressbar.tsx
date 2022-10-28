f9831646434330fcac46d3c6f5e7868735e940da
A Mon <a.mont.24.777@gmail.com>
2022-10-28 18:06:10 -0500

    Agregando funciones para el panel de estilos del main  en el backend

--- blockchain_integration/src/pages/home/progressbar.tsx
+++ blockchain_integration/src/pages/home/progressbar.tsx
@@ -2,6 +2,32 @@ import React, {useState, useEffect} from 'react'
 import { Button as But} from '@chakra-ui/react';
 
 function Progressbar() {
+
+	const [value, setValue] = useState(false);
+
+    const activeInactiveButtontransferstyle = () => {
+   
+		setValue(prevValue => !prevValue );
+		
+		pull_button_transferstyle();
+		
+		};
+
+	const button_signaltransferstyle = value ? 0 : 1
+		
+    const pull_button_transferstyle = async () => {
+		
+			  
+		const res = await fetch('http://localhost:5000/tranferstyle',
+		{ method : "POST", 
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify(button_signaltransferstyle)})
+		const data = await res.json();
+		  
+		}
+
+
+
 	const [filled, setFilled] = useState(0);
 	const [isRunning, setIsRunning] = useState(false);
 	useEffect(() => {
@@ -11,7 +37,7 @@ function Progressbar() {
 	},[filled, isRunning])
   return (
 	  <div>
-		   <But colorScheme= "red"  borderRadius='md' w="100%" h="30px" bg= "red"  onClick={() => {setIsRunning(true)}} type="submit">Style transfer</But>
+		   <But  colorScheme= "red"  borderRadius='md' w="100%" h="30px" bg= "red"  onClick={(e) => {setIsRunning(true); activeInactiveButtontransferstyle();}}> Style transfer</But>
 		  <div className="progressbar">
 			  <div style={{
 				  height: "100%",
