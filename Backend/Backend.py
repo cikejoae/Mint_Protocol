fbdb1c7540afa09e2dba1fd60f7a88708caf3ace
A Mon <a.mont.24.777@gmail.com>
2022-10-28 12:58:21 -0500

    Agregando funcion Gram matrix para procesamiento de estilos al backend

--- Backend/Backend.py
+++ Backend/Backend.py
@@ -124,10 +124,94 @@ class Videocapture():
     
         content_feature = get_features(content, vgg)  
         style_feature = get_features(Style, vgg)
-    
         
+        
+        def gram_matrix(tensor):
+    
+                    _,d , h, w = tensor.size()
+    
+                    tensor = tensor.view( d ,h*w)
+                    gram = torch.mm(tensor,tensor.t())
+    
+                    return gram
+
+
+
+        style_grams = {layer: gram_matrix(style_feature[layer]) for layer in style_feature}
     
     
+        style_weights = {
+                        "conv1_1" : 1,
+                        "conv2_1" : 0.75,
+                        "conv3_1" : 0.2,
+                        "conv4_1" : 0.2,
+                        "conv5_1" : 0.2,
+    
+                            }
+
+        content_weight= 1
+
+        style_weight= 1e6
+
+        target = content.clone().requires_grad_(True).to(device)
+
+        show_every = 25
+
+        optimizer = optim.Adam([target],lr=float(lr))
+
+        steps=25
+
+
+        height, width, channels = im_convert(target).shape
+        image_array=np.empty(shape=(300, height, width, channels ))
+        capture_frame=steps/300
+
+        counter = 0
+
+
+        for ii in range(1, steps + 1):
+    
+                    target_features =get_features(target, vgg)
+    
+                    content_loss = torch.mean((target_features["conv4_2"] - content_feature["conv4_2"])**2)
+    
+                    style_loss=0
+    
+                    for layer in style_weights:
+        
+                        target_feature = target_features[layer]
+                        target_gram = gram_matrix(target_feature)
+        
+                        style_gram = style_grams[layer]
+        
+                        layer_style_loss = style_weights[layer]*torch.mean((target_gram-style_gram)**2)
+                        _, d, h, w = target_feature.shape
+                        style_loss += layer_style_loss/(d*h*w)
+    
+                    total_loss = content_weight*content_loss + style_weight*style_loss
+    
+                    optimizer.zero_grad()
+                    total_loss.backward()
+    
+                    optimizer.step()
+    
+                    if ii%show_every == 0:
+        
+                        print("Total loss: ", total_loss.item())
+                        print("Iteration: ", ii)
+                        
+                        figure = plt.figure()
+                        plt.imshow(im_convert(target))
+                        plt.axis("off")
+                        figure.savefig("../frontend/public/photo.jpg", dpi=300, bbox_inches="tight")
+                        
+        
+                    if ii%capture_frame == 0:
+        
+                        image_array[counter] = im_convert(target)
+                        counter = counter + 1
+    
+     
     
     def Main(self):
         
